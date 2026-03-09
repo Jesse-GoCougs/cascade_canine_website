2ec0afdad87f7941cda34672dee28c51ea4d629b
zolboBatsaikhan <zolboBatsaikhan@gmail.com>
2026-03-09 00:38:45 -0700

    update product page, add product list, add product card

---
+++ components/product-list.tsx
@@ -0,0 +1,23 @@
+import Stripe from "stripe"
+import { ProductCard } from "./product-card";
+
+interface Props {
+    products: Stripe.Product[];
+}
+
+export const ProductList = ({products}: Props) => {
+    return <div>
+        <div>
+            <input type="text" placeholder="Searxh products..."/>
+        </div>
+        <ul>
+            {products.map((product, key) => {
+                return (
+                <li key={key}>
+                    <ProductCard product={product}/>
+                </li>
+                );
+            })}
+        </ul>
+    </div>
+};
